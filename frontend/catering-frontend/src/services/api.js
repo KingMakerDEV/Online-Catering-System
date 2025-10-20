d9bacd5a272f08c0b9cf3338fe8111ac26c1c6ff
Abhay9326397439 <abhayzombade26@gmail.com>
2025-10-20 13:57:59 +0530

    change style and add images

--- frontend/catering-frontend/src/services/api.js
+++ frontend/catering-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',  // ✅ FIXED: Removed extra /api
+  baseURL: 'http://localhost:8081',  // ✅ FIXED: Removed extra /api
   withCredentials: true,
 });
 
